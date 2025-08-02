eaea7b70c1f92688751dbe9bef3068ff8a77e8e6
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-02 16:31:12 +0500

    first commit

---
+++ SwiftUI Bootcamp (Beginner Level)/BackgroundAndOverlayExample.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundAndOverlayExample.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 23/07/25.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundAndOverlayExample()
+}
