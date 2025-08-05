0b0c425baf6e0cc1869d382ea61596506ca24901
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-05 18:16:00 +0500

    example animation

--- SwiftUI Bootcamp (Beginner Level)/AnimationExample.swift
+++ SwiftUI Bootcamp (Beginner Level)/AnimationExample.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct AnimationExample: View {
+    
+    @State var isAnimtion: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Button("Click me") {
+                
+                isAnimtion.toggle()
+
+                
+            }.padding()
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimtion ? 50 : 20)
+                .fill(isAnimtion ? .orange : .black)
+                .frame(
+                    width: isAnimtion ? 100 : 300,
+                    height: isAnimtion ? 100 : 200
+                )
+                .offset(y:isAnimtion ? 100 : 0)
+                .rotationEffect(.degrees(isAnimtion ? 360 : 0))
+                .animation(.linear,value: isAnimtion)
+            Spacer()
+        }
     }
 }
 
