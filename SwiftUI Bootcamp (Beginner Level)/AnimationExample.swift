b286572a16526f85922716805a32360bc4c58321
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-05 18:15:45 +0500

    example animation

---
+++ SwiftUI Bootcamp (Beginner Level)/AnimationExample.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationExample.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 05/08/25.
+//
+
+import SwiftUI
+
+struct AnimationExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationExample()
+}
