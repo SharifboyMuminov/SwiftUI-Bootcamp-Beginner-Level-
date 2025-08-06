69fcacf67cbffffe1868b1d493071aa692bf8389
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-06 10:03:07 +0500

    sheets example

--- SwiftUI Bootcamp (Beginner Level)/SheetsExampleView.swift
+++ SwiftUI Bootcamp (Beginner Level)/SheetsExampleView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct SheetsExampleView: View {
+    
+    @State var showSheet: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.red.ignoresSafeArea()
+            
+            Button {
+                showSheet.toggle()
+            } label: {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(.blue)
+                    .frame(width: 200,height: 70)
+                    .overlay {
+                        Text("Button")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+            }.sheet(isPresented: $showSheet) {
+                SeccondPage()
+            }
+//            .fullScreenCover(isPresented: $showSheet) {
+//                SeccondPage()
+//            }
+
+        }
+    }
+}
+
+
+struct SeccondPage: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack{
+            Color.blue.ignoresSafeArea()
+            
+            Button {
+                presentationMode.wrappedValue
+                    .dismiss()
+            } label: {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(.red)
+                    .frame(width: 200,height: 70)
+                    .overlay {
+                        Text("Exit")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+            }
+
+        }
     }
 }
 
