d87a9c5dae0ac703486a1e47748da3e5ddeee8cd
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-06 10:02:45 +0500

    sheets example

---
+++ SwiftUI Bootcamp (Beginner Level)/SheetsExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetsExampleView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 06/08/25.
+//
+
+import SwiftUI
+
+struct SheetsExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetsExampleView()
+}
