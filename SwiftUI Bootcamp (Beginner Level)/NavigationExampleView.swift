58e92ed32699dbb6ba29401429b26914cb981f92
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-07 18:54:27 +0500

    example NavigationView

---
+++ SwiftUI Bootcamp (Beginner Level)/NavigationExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationExampleView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 07/08/25.
+//
+
+import SwiftUI
+
+struct NavigationExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationExampleView()
+}
