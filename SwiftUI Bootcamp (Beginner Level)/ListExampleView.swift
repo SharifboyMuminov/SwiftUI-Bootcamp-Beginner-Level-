b44339cd4ca966a35ce7c81bd7115d93c17da2de
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-08 17:34:16 +0500

    exaple list

---
+++ SwiftUI Bootcamp (Beginner Level)/ListExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  ListExampleView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 08/08/25.
+//
+
+import SwiftUI
+
+struct ListExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListExampleView()
+}
