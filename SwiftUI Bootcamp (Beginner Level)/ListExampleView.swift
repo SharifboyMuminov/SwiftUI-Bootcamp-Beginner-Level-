81fab6bc9d0f85abb2607eecbf67446be120606e
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-08 17:34:26 +0500

    exaple list

--- SwiftUI Bootcamp (Beginner Level)/ListExampleView.swift
+++ SwiftUI Bootcamp (Beginner Level)/ListExampleView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct ListExampleView: View {
+    @State private var fruits: [String] = [
+        "Apple", "Orange", "Banana", "Pear", "Mango", "Strawberry"]
+
+    
+    @State var greenList: [String] = [
+        "Mint",
+        "Dill",
+        "Cilantro",
+        "Chervil",
+        "Parsley",
+        "Arugula",
+        "Lettuce leaf",
+        "Spinach",
+        "Cabbage leaf",
+        "Horsetail",
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List{
+                Section(header: Text("Fruits")){
+                    ForEach(fruits, id: \.self){ fruit in
+                        Text(fruit)
+                    }
+                    .onMove(perform: moveFruits)
+                    .onDelete(perform: deleteFruit)
+                    
+                }
+                
+                Section(header: Text("Greens")){
+                    ForEach(greenList, id: \.self){ green in
+                        Text(green)
+                    }
+                    .onDelete(perform: deleteGreens)
+                    .onMove(perform: moveGreens)
+                }
+                
+            }
+            .navigationTitle("Shop")
+            .navigationBarItems(leading: EditButton())
+        }
+    }
+    
+    func moveFruits( indexSet: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indexSet, toOffset: newOffset)
+    }
+    
+    func moveGreens( indexSet: IndexSet, newOffset: Int) {
+        greenList.move(fromOffsets: indexSet, toOffset: newOffset)
+    }
+    
+    func deleteFruit(at offsets: IndexSet) {
+        fruits.remove(atOffsets: offsets)
+    }
+    
+    func deleteGreens(at offsets: IndexSet) {
+        greenList.remove(atOffsets: offsets)
     }
 }
 
