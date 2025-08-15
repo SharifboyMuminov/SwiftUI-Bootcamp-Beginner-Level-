e7e30726ece9f686a7c6c669a0363765f4583537
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-15 09:17:38 +0500

    ContextMenuExamapleView

--- SwiftUI Bootcamp (Beginner Level)/ContextMenuExamapleView.swift
+++ SwiftUI Bootcamp (Beginner Level)/ContextMenuExamapleView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ContextMenuExamapleView: View {
+    
+    @State var backColor: Color = .blue
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Menu("Change Color") {
+                         Button("Color red") {
+                             backColor = .red
+                         }
+                         Button("Color blue") {
+                             backColor = .blue
+                         }
+                     }
+                     .padding()
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(backColor)
+                .frame(width: 200, height: 200)
+                .contextMenu {
+                    Button("Color red") {
+                        self.backColor = .red
+                        
+                    }
+                    Button("Color blue") {
+                        self.backColor = .blue
+                        
+                    }
+                }
+        }
+        
+        
     }
 }
 
