fad8461086f6459985d57658dae3721086d89724
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-15 09:49:38 +0500

    PickerExampleView

---
+++ SwiftUI Bootcamp (Beginner Level)/PickerExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerExampleView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 15/08/25.
+//
+
+import SwiftUI
+
+struct PickerExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerExampleView()
+}
