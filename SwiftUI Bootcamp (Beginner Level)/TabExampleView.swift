b03b63a7d4fd4724f29d21270be99b9cfd9e4d12
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-15 09:58:48 +0500

    TabView example

--- SwiftUI Bootcamp (Beginner Level)/TabExampleView.swift
+++ SwiftUI Bootcamp (Beginner Level)/TabExampleView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct TabExampleView: View {
+    @State var selectedTab: Int = 0
+    
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab) {
+            HomeView(selectTabValue: $selectedTab)
+                .tabItem {
+                    Image(systemName: "house")
+                }.tag(0)
+            
+            Text("Tab 2")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.red)
+                .font(.headline)
+                .tabItem {
+                Image(systemName: "briefcase.fill")
+                
+            }.tag(1)
+            
+            Text("Profile")
+                .font(.headline)
+                .tabItem {
+                Image(systemName: "person.crop.circle")
+            }.tag(2)
+        }
+    }
+}
+
+struct HomeView: View {
+    @Binding var selectTabValue: Int
+    
+    var body: some View {
+        VStack{
+            Button("Go to profile") {
+                selectTabValue = 2
+            }
+        }
     }
 }
 
