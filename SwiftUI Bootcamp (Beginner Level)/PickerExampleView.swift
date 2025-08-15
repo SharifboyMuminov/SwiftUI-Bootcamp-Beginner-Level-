53a36bda958b062e63d5969e7eec918a1642de36
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-15 09:49:48 +0500

    PickerExampleView

--- SwiftUI Bootcamp (Beginner Level)/PickerExampleView.swift
+++ SwiftUI Bootcamp (Beginner Level)/PickerExampleView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct PickerExampleView: View {
+    @State var pickerSelectionValue: Int = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Active vale: \(pickerSelectionValue)")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Picker(selection: $pickerSelectionValue) {
+                ForEach(1...20, id: \.self) { digit in
+                    Text("Hello, World! \(digit)").tag(digit)
+                }
+            } label: {
+                Text("Digits")
+            }
+            .pickerStyle(WheelPickerStyle())
+            
+            Picker(selection: $pickerSelectionValue) {
+                ForEach(1...3, id: \.self) { digit in
+                    Text("\(digit)")
+                }
+            } label: {
+                Text("Digits")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+
+
     }
 }
 
