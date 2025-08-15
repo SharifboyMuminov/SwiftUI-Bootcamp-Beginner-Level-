603e96ac2782cc57417405408e995d16d64f8b28
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-08-15 09:58:42 +0500

    TabView example

---
+++ SwiftUI Bootcamp (Beginner Level)/TabExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  TabExampleView.swift
+//  SwiftUI Bootcamp (Beginner Level)
+//
+//  Created by Muminov Sharifjon on 15/08/25.
+//
+
+import SwiftUI
+
+struct TabExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabExampleView()
+}
